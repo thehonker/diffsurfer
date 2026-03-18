45c415c6fcb8c73de041eb03a1e7c04c49390d10
Edwin Pers <pers.edwin@gmail.com>
2026-03-18 16:13:51 -0400

    Fix Homebrew packaging: Convert from Formula to Cask for GUI application

---
+++ Casks/diffsurfer.rb
@@ -0,0 +1,15 @@
+cask "diffsurfer" do
+  version "1.1.2"
+  sha256 "0c4365466eb68770bac0fadb84a1cb3fcc4cb44fc1df959948798764829919d2"
+
+  url "https://github.com/thehonker/diffsurfer/releases/download/v#{version}/Diffsurfer-#{version}-arm64.dmg"
+  name "Diffsurfer"
+  desc "A GUI for viewing commit history timelines"
+  homepage "https://github.com/thehonker/diffsurfer"
+
+  app "Diffsurfer.app"
+
+  caveats do
+    requires_rosetta
+  end
+end
