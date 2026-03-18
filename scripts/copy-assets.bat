a60f17e40ecb88a1d176f641cf1760a9a68fe1a2
Edwin Pers <pers.edwin@gmail.com>
2026-03-18 15:27:37 -0400

    fix copy-assets invocation in package.json, make copy-assets.bat more robust

--- scripts/copy-assets.bat
+++ scripts/copy-assets.bat
@@ -1,23 +1,26 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: Change to the script's directory to ensure relative paths work correctly
+cd /d "%~dp0"
+
 :: Create dist directory if it doesn't exist
-if not exist "dist" mkdir "dist"
+if not exist "..\dist" mkdir "..\dist"
 
 :: Read from assets.csv and copy files
-for /f "tokens=1,2 delims=," %%a in (assets.csv) do (
+for /f "tokens=1,2 delims=," %%a in (..\assets.csv) do (
     set "src=%%a"
     set "dest=%%b"
     
+    :: Remove any quotes from src and dest
+    set "src=!src:"=!"
+    set "dest=!dest:"=!"
+    
     :: Create destination directory
-    for %%f in ("!dest!") do (
+    for %%f in ("..\!dest!") do (
         if not exist "%%~dpf" mkdir "%%~dpf"
     )
     
     :: Copy files
-    if "!src!" == "*" (
-        xcopy "!src!" "!dest!" /E /I /Y
-    ) else (
-        xcopy "!src!" "!dest!" /E /I /Y
-    )
+    xcopy "..\!src!" "..\!dest!" /E /I /Y /Q
 )
