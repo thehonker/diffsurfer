a35935295f296bfdfe32ac5b094fb64f9a40064b
Edwin Pers <pers.edwin@gmail.com>
2026-03-18 15:30:52 -0400

    v1.0.3 - another swing at fixing windows builds

--- scripts/copy-assets.bat
+++ scripts/copy-assets.bat
@@ -1,14 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Change to the script's directory to ensure relative paths work correctly
-cd /d "%~dp0"
+:: Change to the project root directory to ensure paths work correctly
+cd /d "%~dp0.."
 
 :: Create dist directory if it doesn't exist
-if not exist "..\dist" mkdir "..\dist"
+if not exist "dist" mkdir "dist"
 
 :: Read from assets.csv and copy files
-for /f "tokens=1,2 delims=," %%a in (..\assets.csv) do (
+for /f "tokens=1,2 delims=," %%a in (assets.csv) do (
     set "src=%%a"
     set "dest=%%b"
     
@@ -16,11 +16,19 @@ for /f "tokens=1,2 delims=," %%a in (..\assets.csv) do (
     set "src=!src:"=!"
     set "dest=!dest:"=!"
     
-    :: Create destination directory
-    for %%f in ("..\!dest!") do (
-        if not exist "%%~dpf" mkdir "%%~dpf"
+    :: Handle wildcard copies differently
+    echo !src! | findstr /C:"*" >nul
+    if !errorlevel! equ 0 (
+        :: This is a wildcard copy
+        for %%f in ("!dest!") do (
+            if not exist "%%~dpf" mkdir "%%~dpf"
+        )
+        robocopy "!src!" "!dest!" /E /NFL /NDL /NJH /NJS /NP /XC /XN /XO
+    ) else (
+        :: This is a single file or directory copy
+        for %%f in ("!dest!") do (
+            if not exist "%%~dpf" mkdir "%%~dpf"
+        )
+        xcopy "!src!" "!dest!" /E /I /Y /Q
     )
-    
-    :: Copy files
-    xcopy "..\!src!" "..\!dest!" /E /I /Y /Q
 )
