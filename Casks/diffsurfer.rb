9f908f58b63f2320950c18f6d017bd3bdcff7d43
Edwin Pers <pers.edwin@gmail.com>
2026-03-18 18:38:18 -0400

    chore: bump version to 1.1.13

--- Casks/diffsurfer.rb
+++ Casks/diffsurfer.rb
@@ -1,5 +1,5 @@
 cask "diffsurfer" do
-  version "1.1.12"
+  version "1.1.13"
   sha256 "090e59322b676bf392ad2d4b453de13373528f2256cba64066b17e875b77240d"
 
   url "https://github.com/thehonker/diffsurfer/releases/download/v#{version}/Diffsurfer-#{version}-arm64.dmg"
